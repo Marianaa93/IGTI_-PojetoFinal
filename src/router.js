bb5e8a12cbb06f369b0591d5776d7ab564b9a086
Mariana Lima <marianalima.amb@gmail.com>
2022-10-23 21:12:55 -0300

    workinh

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import SolicitacoesRecebidas from './pages/solicitacao/SolicitacoesRecebidas';
+import TutorContato from './pages/solicitacao/TutorContato';
+import TutorDetalhes from './pages/tutores/TutorDetalhes';
+import TutorRegistro from './pages/tutores/TutorRegistro';
+import TutoresLista from './pages/tutores/TutoresLista'
+import NotFound from './pages/NotFound.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/tutores' },
+    {
+      path: '/tutores', component: TutoresLista
+    },
+
+    {
+      path: '/tutores/:id', component: TutorDetalhes,
+      children: [
+        { path: 'contato', component: TutorContato }
+      ],
+    },
+    { path: '/registrar', component: TutorRegistro },
+    { path: '/solicitacao', component: SolicitacoesRecebidas },
+    { path: '/:notFound(.*)', component: NotFound },
+  ]
+});
+
+export default router;
